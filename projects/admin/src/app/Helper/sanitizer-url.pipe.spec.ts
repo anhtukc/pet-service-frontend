d612ac9d1c3bfd10a59f8d02ba035984094ab4f9
anhtukc <anhtukc2.0@gmail.com>
2023-05-28 13:23:38 +0700

    v0.0.6

--- projects/admin/src/app/Helper/sanitizer-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SanitizerUrlPipe } from './sanitizer-url.pipe';
-
-describe('SanitizerUrlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SanitizerUrlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
